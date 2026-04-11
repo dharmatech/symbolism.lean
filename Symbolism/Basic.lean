bb37824d92704ff257fee9a47c7c82dbca560205
dharmatech <wayo.cavazos@gmail.com>
2026-04-11 07:41:12 -0700

    Remove redundant cases in mk_add and mk_mul functions for cleaner expression handling

--- Symbolism/Basic.lean
+++ Symbolism/Basic.lean
@@ -53,7 +53,6 @@ def normalize_add_terms (terms : List Expr) : List Expr :=
     rest
   else .rat const :: rest
 
-
 def normalize_mul_factors (factors : List Expr) : List Expr :=
   if factors.any (fun e => e == .rat 0) then
     [.rat 0]
@@ -71,26 +70,8 @@ def normalize_mul_factors (factors : List Expr) : List Expr :=
     else
       .rat const :: rest
 
-
-
--- def mk_add (a b : Expr) : Expr :=
---   match a, b with
---   | .rat 0,      e => e
---   |      e, .rat 0 => e
---   | .rat m, .rat n => .rat (m + n)
---   |      _,      _ => .add a b
-
 def mk_add (a b : Expr) : Expr :=
   from_add_terms (normalize_add_terms (add_terms a ++ add_terms b))
-
--- def mk_mul (a b : Expr) : Expr :=
---   match a, b with
---   | .rat 0,      _ => .rat 0
---   |      _, .rat 0 => .rat 0
---   | .rat 1,      e => e
---   |      e, .rat 1 => e
---   | .rat m, .rat n => .rat (m * n)
---   |      _,      _ => .mul a b
 
 def mk_mul (a b : Expr) : Expr :=
   from_mul_factors (normalize_mul_factors (mul_factors a ++ mul_factors b))
